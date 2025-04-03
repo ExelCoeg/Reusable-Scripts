1f24bb765fd288792a081a5eaf9c115f31195892
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2025-04-03 15:53:10 +0700

    making UIManager more scalable

--- Managers/UIManager.cs
+++ Managers/UIManager.cs
@@ -1,20 +1,81 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIManager : SingletonMonoBehaviour<UIManager>
 {
+
+
+    /*
+        FOR ANIMS, CREATE ANOTHER SCRIPT THAT MANAGES UI ANIMS
+    */
     public bool isDebug;
+
+    private InputSystem_Actions inputActions;
+    private InputAction pause;
+    
+    [ReadOnly]
     public UI currentUI;
 
     public bool enableCursor = true;
 
+    Dictionary<UI,UIBase> uiInstances = new Dictionary<UI,UIBase>();
+
     #region UIPrefabs
+
+    [Title("Prefabs")] 
+    public UIPause uiPausePrefab;
+    public UIPhone uiPhonePrefab;
+    #endregion
+
+
+    [Title("Canvas Reference")]
+    public Transform canvas;
+
     
+    #region Input Actions
+    private void OnEnable() {
+        inputActions = new InputSystem_Actions();
+
+        EnableUIInput();
+        
+        pause = inputActions.UI.Pause;
+        pause.performed += ctx => {
+            if(GameManager.instance.isPaused) {
+                GameManager.instance.Resume();
+                ShowUI(UI.GAMEPLAY);
+            }
+            else {
+                ShowUI(UI.PAUSE);
+                GameManager.instance.Pause();
+            }
+        };
+    }
+
+    private void OnDisable()
+    {
+        DisableUIInput();
+    }
+
+    public void EnableUIInput(){
+        inputActions.UI.Enable();
+    }
+
+    public void DisableUIInput(){
+        inputActions.UI.Disable();
+    }
+
     #endregion
 
-    #region UIReferences
+    
+    void Start()
+    {   
+        InitializeUI(UI.PAUSE, uiPausePrefab);
+        InitializeUI(UI.GAMEPLAY, null);
 
-    #endregion
-
+        HideUI(UI.PAUSE);
+    }
     private void Update() {
         Cursor.visible = enableCursor;
         if(currentUI == UI.GAMEPLAY){
@@ -24,62 +85,41 @@ public class UIManager : SingletonMonoBehaviour<UIManager>
             Cursor.lockState = CursorLockMode.None;
         }
     }
-
+    private void InitializeUI(UI ui, UIBase uiPrefab){
+        if(uiPrefab != null){
+            uiInstances[ui] = Instantiate(uiPrefab,canvas.transform);
+        }
+        else{
+            uiInstances[ui] = null;
+        }
+    }
     public void ShowUI(UI ui){
-        #if ENABLE_DEBUG
-            Debug.Log("Showing UI: " + ui);
-        #endif
         if(currentUI == ui){
             return;
         }
+
         HideUI(currentUI);
 
-        switch(ui){
-            // case UI.PAUSE:
-            //     uiPause.Show();
-            //     break;
-            // case UI.BOOK:
-            //     uiBook.Show();
-            //     break;
-            // case UI.SETTINGS:
-            //     uiSettings.Show();
-            //     break;
+        if(uiInstances.TryGetValue(ui, out UIBase uiInstance)){
+            uiInstance.Show();
         }
+
         currentUI = ui;
     }
 
     public void HideUI(UI ui){
-        #if ENABLE_DEBUG
-            Debug.Log("Hiding UI: " + ui);
-        #endif
-
         if(currentUI != ui){
             return;
         }
-        switch(ui){
-            // case UI.PAUSE:
-            //     uiPause.Hide();
-            //     break;
-            // case UI.BOOK:
-            //     uiBook.Hide();
-            //     break;
-            // case UI.SETTINGS:
-            //     uiSettings.Hide();
-            //     break;
+        if(uiInstances.TryGetValue(ui, out UIBase uiInstance)){
+            uiInstance.Hide();
         }
         currentUI = UI.GAMEPLAY;
     }
     public enum UI{
         PAUSE,
         GAMEPLAY,
-        SETTINGS
     }
-     public void OnEnable(){
-        // inputActions = new PlayerInputActions();
-        // pauseAction = inputActions.UI.Pause;
-        // pauseAction.Enable();
-        // pauseAction.performed += ctx => {
-            
-        // };
-    }
+
+
 }
